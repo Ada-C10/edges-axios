eee9ab2387b7d09f79ef070a121c2ebe8dfaab25
Dan Roberts <dan@adadevelopersacademy.org>
2018-11-20 11:13:48 -0800

    Status reporting

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 // index.js
 const URL = 'https://petdibs.herokuapp.com/pets';
 
+const reportStatus = (message) => {
+  $('#status-message').html(message);
+};
+
 const loadPets = () => {
+  reportStatus("loading pets...");
+
   // Prep work
   const petList = $('#pet-list');
   petList.empty();
@@ -9,11 +15,16 @@ const loadPets = () => {
   // Actually load the pets
   axios.get(URL)
     .then((response) => {
+      reportStatus(`Successfully loaded ${response.data.length} pets`);
+
+      // sort the pets
+
       response.data.forEach((pet) => {
         petList.append(`<li>${pet.name}</li>`);
       });
     })
     .catch((error) => {
+      reportStatus(error);
       console.log(error);
     });
 };
